82abd1893264472392ad10f3bdbbc1d4e11e1490
Nerses Arslanian <nerses.arslanian@gmail.com>
2025-12-21 20:34:48 -0800

    cloud infra.

---
+++ universes/cloud_infrastructure.py
@@ -0,0 +1,15 @@
+
+"""
+Cloud Infrastructure Universe
+
+Companies with material cloud infrastructure (IaaS / PaaS) businesses.
+Companies may also appear in other business-model universes.
+"""
+
+CLOUD_INFRASTRUCTURE = [
+    "AMZN",   # AWS
+    "MSFT",   # Azure
+    "GOOGL",  # Google Cloud
+    "IBM",    # IBM Cloud
+    "ORCL",   # Oracle Cloud
+]
